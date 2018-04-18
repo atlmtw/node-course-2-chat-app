ef47f1b3a3e0030cbc7d2c5a33bbccd5f378f3c7
Mason Wong <masontywong@gmail.com>
2018-04-17 23:46:29 -0400

    Greet new user, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,9 @@ var socket = io();
 
 socket.on('connect', function(){
   console.log('Connected to server');
-
+  // socket.on('newMessage', function(message){
+  //   console.log(message);
+  // });
 });
 
 socket.on('disconnect', function(){
--- server/server.js
+++ server/server.js
@@ -13,13 +13,31 @@ app.use(express.static(path.join(__dirname,'../public')));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to the chat app',
+    createdAt:new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New User Joined',
+    createdAt:new Date().getTime()
+  });
+
   socket.on('createMessage',(newMessage)=>{
-    console.log('createMessage',newMessage);
     io.emit("newMessage", {
       from: newMessage.from,
       text: newMessage.text,
       createdAt:new Date().getTime()
     })
+
+    //everyone but this socket is what broadcast does
+    // socket.broadcast.emit('newMessage',{
+    //   from:newMessage.from,
+    //   text:newMessage.text,
+    //   createdAt:new Date().getTime()
+    // });
   })
 
   socket.on('disconnect', () => {
