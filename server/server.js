431b7c20b51b1e3ceb186716999433de8bc85259
Mason Wong <masontywong@gmail.com>
2018-04-23 00:10:12 -0400

    Format timestamps using momentjs

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ io.on('connection', (socket) => {
 
   socket.on('createLocationMessage',(coords)=>{
     io.emit("newLocationMessage",generateLocationMessage(
-      'Admin: ',
+      'Admin',
       coords.latitude,
       coords.longitude
     ));
